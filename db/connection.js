b68c38ad64a587b8af46897527920a0797629319
Aidan Roche <aidanroche937@gmail.com>
2023-10-11 20:46:34 -0400

    Added personal DB

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,7 @@ class Connection {
     dotenvConfig();
 
     const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_CLUSTER } = process.env;
-    const DATABASE_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_CLUSTER}.xtdufxk.mongodb.net/?retryWrites=true&w=majority`;
+    const DATABASE_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_CLUSTER}.vyjpl5l.mongodb.net/?retryWrites=true&w=majority`;
 
     mongoose.connect(DATABASE_URL, {
       useNewUrlParser: true,
--- db_accessor/user.accessor.js
+++ db_accessor/user.accessor.js
@@ -4,7 +4,7 @@ import User from "../models/user.js";
 class UserAccessor {
   static async getAllUsers() {
     try {
-      await Connection.open("users");
+      await Connection.open("Network-Builder");
       const users = [];
       for await (const doc of User.find()) {
         users.push(doc);
