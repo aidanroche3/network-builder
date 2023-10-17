e03a460fe72657a605d77a0895c9f29fe6645d54
Aidan Roche <aidanroche937@gmail.com>
2023-10-16 20:14:21 -0400

    Added Login and Sign-Up pages

--- routes/page.routes.js
+++ routes/page.routes.js
@@ -1,17 +1,22 @@
 import express from "express";
 import path from "path";
 import UserController from "../controllers/user.controller.js";
+import User from "../models/user.js";
 
 const router = express.Router();
 
 router.route("/").get(UserController.getAllUsers);
 
-router.route("/page2").get((req, res) => {
-  res.sendFile(path.resolve() + "/page2.html");
-});
-
 router.route("/css/index.css").get((req, res) => {
   res.sendFile(path.resolve() + "/css/index.css");
 });
+
+router.route("/css/style.css").get((req, res) => {
+  res.sendFile(path.resolve() + "/css/style.css");
+});
+
+router.route("/login-page").get(UserController.getLoginPage);
+
+router.route("/sign-up").get(UserController.getSignUpPage);
 
 export default router;
